cf37ffb391d07af01603e58954c09d639fea9804
Vienio99 <kyve96@gmail.com>
2021-03-28 17:05:46 +0200

    added more css

--- pokedex/forms.py
+++ pokedex/forms.py
@@ -1,5 +1,7 @@
 from django import forms
+from django.forms import Textarea
 from .models import Comment
+from django.utils.translation import gettext_lazy as _
 
 class CommentForm(forms.ModelForm):
 
@@ -9,4 +11,8 @@ class CommentForm(forms.ModelForm):
         labels = {
             'comment': (''),
         }
+        widgets = {
+            'comment': Textarea(attrs={'cols': 80, 'rows': 5, 'placeholder': 'Enter your comment here', 'class': 'comment-form'}),
+        }
+            
     
